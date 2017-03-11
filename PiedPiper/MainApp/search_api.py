114365a6ad0b12893bd0ebe7bf6809cd4a6b9553
Hari Dahal <hxd1478@rit.edu>
2017-03-11 01:29:42 -0500

    Search API object to connect with Apple

---
+++ PiedPiper/MainApp/search_api.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class SearchAPI:
+
+    def __init__(self):
+        self.api_url = "https://itunes.apple.com/search?"
+
+    def search_term(self, term):
+        url = "term="+"+".join(term.split())
+        return self.call_api(url)
+
+    def call_api(self, url):
+        the_url = self.api_url + url
+        response = requests.get(the_url)
+        return response.json()
