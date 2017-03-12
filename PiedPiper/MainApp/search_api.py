de3d33610ffa24cf8fe82beefbe74e3fead97f4c
Hari Dahal <hxd1478@rit.edu>
2017-03-12 08:19:35 -0400

    catch ConnectionError if offline

--- PiedPiper/MainApp/search_api.py
+++ PiedPiper/MainApp/search_api.py
@@ -21,7 +21,12 @@ class SearchAPI:
 
     def call_api(self, url):
         the_url = url
-        response = requests.get(the_url)
-        data = response.json()['results']
-        return data
+        try:
+            response = requests.get(the_url)
+            data = response.json()
+            if 'results' in data:
+                data = data['results']
+            return data
+        except requests.exceptions.ConnectionError:
+            return 'Connection Error'
 
