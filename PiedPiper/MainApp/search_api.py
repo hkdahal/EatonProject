77434afacc422e6dd9a0f4a370a3c08ab244ffcf
Hari Dahal <hxd1478@rit.edu>
2017-03-12 23:54:21 -0500

    arranged static methods as necessarily

--- PiedPiper/MainApp/search_api.py
+++ PiedPiper/MainApp/search_api.py
@@ -1,4 +1,24 @@
+import json
 import requests
+
+
+def make_request(url):
+    try:
+        return 'success', requests.get(url)
+    except requests.exceptions.ConnectionError:
+        return 'connection error', None
+
+
+def call_api(url):
+
+    response = make_request(url)
+
+    if response[0] == 'success':
+        data = response[1].json()
+        if 'results' in data:
+            return data['results']
+    elif response[0] == 'connection error':
+        return 'Connection Error'
 
 
 class SearchAPI:
@@ -7,26 +27,14 @@ class SearchAPI:
         self.api_search_url = "https://itunes.apple.com/search?"
         self.api_lookup_url = "http://itunes.apple.com/lookup?"
 
-    def search_term(self, term):
-        url = "term="+"+".join(term.split())
-        return self.call_api(url)
-
     def search_artist(self, term):
-        url = self.api_search_url + "term="+"+".join(term.split()) + "&entity=musicTrack"
-        return self.call_api(url)
+        url = self.make_search_url(term)
+        return call_api(url)
 
     def lookup_id(self, given_id):
         url = self.api_lookup_url + "id=" + given_id
-        return self.call_api(url)
+        return call_api(url)
 
-    def call_api(self, url):
-        the_url = url
-        try:
-            response = requests.get(the_url)
-            data = response.json()
-            if 'results' in data:
-                data = data['results']
-            return data
-        except requests.exceptions.ConnectionError:
-            return 'Connection Error'
-
+    def make_search_url(self, term):
+        return self.api_search_url + "term="+"+".join(term.split()) + \
+               "&entity=musicTrack"
